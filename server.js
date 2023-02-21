b36d17e8e004cd366aec0ceeae4d9c93e01153f3
Luiza0607 <110295222+Luiza0607@users.noreply.github.com>
2023-02-21 21:25:58 +0100

    add contact page validation

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 const hbs = require('express-handlebars');
 
 const app = express();
-app.engine('hbs', hbs());
-app.set('view engine', 'hbs');
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/hello/:name', (req, res) => {
-	res.render('hello');
+	res.render('hello', { layout: false, name: req.params.name });
 });
 
 app.get('/about', (req, res) => {
@@ -32,9 +32,22 @@ app.get('/history', (req, res) => {
 	res.render('history');
 });
 
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/contact/send-message', (req, res) => {
+	const { author, sender, title, message } = req.body;
+
+	if (author && sender && title && message) {
+		res.render('contact', { isSent: true });
+	}
+	else {
+		res.render('contact', { isError: true });
+	}
+});
+
 app.use((req, res) => {
 	res.status(404).send('404 not found...');
-})
+});
 
 app.listen(8000, () => {
 	console.log('Server is running on port: 8000');
